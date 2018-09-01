d66f7eb03fe84eaf068438a97285823a551cf6e1
peterlundblad <cfude88@gmail.com>
2018-09-01 12:58:05 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Greetings Humans</h1>
+    <p>I am Peter, from the Planet Vultron.</p>
+    <p>Can I has a cheezcak?</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
